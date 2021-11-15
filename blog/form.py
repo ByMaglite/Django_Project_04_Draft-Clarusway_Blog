c307e1da2b86c73b34dadde8902b3b60f20ea4f9
ByMaglite <fceyhan55@gmail.com>
2021-11-15 17:58:24 +0300

    form file finished

---
+++ blog/form.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Post, Comment,Category
+
+
+class PostForm(forms.ModelForm):
+    status = forms.ChoiceField(choices=Post.OPTIONS)
+    category = forms.ModelChoicesField(queryset = Category.objects.all(),empty_label='Select')
+    class Meta:
+        model = Post
+        fields = (
+            'title',
+            'image',
+            'content',
+            'category',
+            'status',
+        )
+
+
+class CommentForm(forms.ModelForm):
+    clss Meta:
+        model = Comment
+        fields = ('content',)
