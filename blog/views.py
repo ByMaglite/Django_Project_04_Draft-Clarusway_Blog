cb05c72f0a66696f43c03bd02c05109bae5785b0
ByMaglite <fceyhan55@gmail.com>
2021-11-16 12:04:12 +0300

    views finished

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    gs = Post.objects.all()
+    context = {
+        "objects_list":gs
+    }
+    return render(request,"blog/post_list.html", context)
